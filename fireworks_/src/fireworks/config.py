c012add5f6e0834a8e369973cfee90c42355d221
Ilya <sovailya2208@gmail.com>
2026-03-12 17:44:15 +0300

    firework_ v 1.1

--- fireworks_/src/fireworks/config.py
+++ fireworks_/src/fireworks/config.py
@@ -10,8 +10,8 @@ class Config():
 
     g: float = 9.81                             # ускорение свободного падения
 
-    v_min: float = 0.0                          # минимальная начальная скорость
-    v_max: float = 16.0                         # базовая начальная скорость
+    v_min: float = 0.0                          # начальная скорость
+    v_max: float = 16.0
 
     num_particles_min: int = 150                # число частиц
     num_particles_max: int = 200
@@ -26,7 +26,7 @@ class Config():
 
     confetti_frames: list[str] = field(
         default_factory=lambda: list("()|\\?/.!")
-    )
+    )                                           # символы конфетти
 
     confetti_init_coef_for_vy: float = 0.5
     confetti_init_vy: float = field(init = False)
@@ -55,13 +55,19 @@ class Config():
         default_factory=lambda: [[6,9,12], [0], [0,3], [18], [12,15,18], [0,3,6,9,12,15,18], [21], [24], [27]]
     )
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if self.fps <= 0:
-            raise ValueError("fps must be > 0")
+            raise ValueError("fps must be greater than 0")
         if self.scale_x <= 0 or self.scale_y <= 0:
-            raise ValueError("scale_x and scale_y must be > 0")
+            raise ValueError("scale_x and scale_y must be greater than 0")
         if self.v_min > self.v_max:
-            raise ValueError("v_min should not greater that v")
+            raise ValueError("v_min must be less than or equal to v_max")
+        if self.num_particles_min > self.num_particles_max:
+            raise ValueError("num_particles_min must be less than or equal to num_particles_max")
+        if self.confetti_lifetime_min > self.confetti_lifetime_max:
+            raise ValueError("confetti_lifetime_min must be less than or equal to confetti_lifetime_max")
+        if self.tail_len_min > self.tail_len_max:
+            raise ValueError("tail_len_min must be less than or equal to tail_len_max")
         self.dt = 1.0 / self.fps
         self.confetti_init_vy = self.g * self.confetti_init_coef_for_vy
 
